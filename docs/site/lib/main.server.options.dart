7f06554cf1f591aca208391d617d79fe04aa5341
Ifiok Jr. <ifiokotung@gmail.com>
2026-06-23 13:09:46 +0100

    docs: add site search

--- docs/site/lib/main.server.options.dart
+++ docs/site/lib/main.server.options.dart
@@ -50,11 +50,11 @@ ServerOptions get defaultServerOptions => ServerOptions(
     ),
   },
   styles: () => [
-    ..._zoomable_image.ZoomableImage.styles,
     ..._callout.Callout.styles,
     ..._github_button.GitHubButton.styles,
     ..._image.Image.styles,
     ..._theme_toggle.ThemeToggleState.styles,
+    ..._zoomable_image.ZoomableImage.styles,
   ],
 );
 
