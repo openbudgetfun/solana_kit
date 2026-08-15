a5492c853c5b597d24328f2397c8cfecad2fa84d
Ifiok Jr. <ifiokotung@gmail.com>
2026-08-15 14:03:28 +0100

    style(helius): const in test

--- packages/solana_kit_helius/test/auth/payments_test.dart
+++ packages/solana_kit_helius/test/auth/payments_test.dart
@@ -156,7 +156,7 @@ void main() {
     test('payPaymentLink converts cents to raw USDC', () async {
       final client = _rpcAndSenderClient();
       final rpc = JsonRpcClient(url: 'https://rpc', client: client);
-      final link = PaymentLink(
+      final link = const PaymentLink(
         kind: 'payment_required',
         paymentIntentId: 'pi-1',
         amountCents: 100,
